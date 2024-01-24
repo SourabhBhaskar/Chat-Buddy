00ad1ffb8c20491ceeb4d30e3da0ccd699c97eb9
Sourabh Bhaskar <sourabhbhaskar71@gmail.com>
2024-01-24 11:00:48 +0530

    deploy-6

--- server/middlewares/isUserAuthenticated.middleware.js
+++ server/middlewares/isUserAuthenticated.middleware.js
@@ -3,7 +3,7 @@
 function isUserAuthenticated(req, res, next) {
   if(!req.isAuthenticated()){
     console.log("User is not authenticated")
-    return res.status(400).json({ error: "User is not authenticated"});
+    return res.status(400).send("User is not authenticated");
   }
   next();
  }
